19af510eb0a81574a419beb9f53b87d75c296420
Lena Garber <lgarber@akamai.com>
2024-08-31 18:12:27 -0400

    oops

--- linodecli/documentation/template_data.py
+++ linodecli/documentation/template_data.py
@@ -6,7 +6,7 @@ from dataclasses import dataclass, field
 from io import StringIO
 from typing import Dict, List, Optional, Self
 
-from linodecli import CLI
+from linodecli.cli import CLI
 from linodecli.baked import OpenAPIOperation
 from linodecli.baked.operation import OpenAPIOperationParameter
 from linodecli.baked.request import OpenAPIRequestArg
